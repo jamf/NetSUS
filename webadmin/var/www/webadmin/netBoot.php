a428fba2a3a6b77f13dbb277b036beac9dee5af8
Josh Schripsema <jschripsema@expedia.com>
2014-05-27 13:23:18 -0700

    Set NetBootImage when Enabling
    Fix bug that did not properly set the NetBoot image when enabling
    NetBoot.

--- webadmin/var/www/webadmin/netBoot.php
+++ webadmin/var/www/webadmin/netBoot.php
@@ -12,11 +12,36 @@ $currentIP = trim(getCurrentIP());
 
 $netbootimgdir = "/srv/NetBoot/NetBootSP0/";
 
-
-if (isset($_POST['enablenetboot']))
+if (isset($_POST['NetBootImage']))
 {
-	suExec("setnbimages ".$nbi);
+	$wasrunning = getNetBootStatus();
+	$nbi = $_POST['NetBootImage'];
+	if ($nbi != "")
+	{
+		$nbconf = file_get_contents("/var/appliance/conf/dhcpd.conf");
+		$nbsubnets = "";
+		foreach($conf->getSubnets() as $key => $value)
+		{
+			$nbsubnets .= "subnet ".$value['subnet']." netmask ".$value['netmask']." {\n\tallow unknown-clients;\n}\n\n";
+		}
+		$nbconf = str_replace("##SUBNETS##", $nbsubnets, $nbconf);
+		suExec("touchconf \"/var/appliance/conf/dhcpd.conf.new\"");
+		if(file_put_contents("/var/appliance/conf/dhcpd.conf.new", $nbconf) === FALSE)
+		{
+			echo "<div class=\"errorMessage\">ERROR: Unable to update dhcpd.conf</div>";
+		 
+		}
+		suExec("disablenetboot");
+		suExec("installdhcpdconf");
+	
+		if ($wasrunning || isset($_POST['enablenetboot']))
+		{
+			suExec("setnbimages ".$nbi);
+		}
+		$conf->setSetting("netbootimage", $nbi);
+	}
 }
+
 if (isset($_POST['disablenetboot']))
 {
 	suExec("disablenetboot");
