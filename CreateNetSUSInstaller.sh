7bfc538377dedd50d2f7f34595dd747ffdb5bd9b
Duncan McCracken <duncan.mccracken@mondada.com.au>
2017-06-12 14:02:20 +1000

    Re-instated deprecated commands (Commented out)

--- CreateNetSUSInstaller.sh
+++ CreateNetSUSInstaller.sh
@@ -15,6 +15,17 @@ timeEcho "Building NetSUSLP Installer..."
 rm -f NetSUSInstaller.run 2>&1 > /dev/null
 rm -Rf temp 2>&1 > /dev/null
 
+#mkdir temp
+#cp -R base temp
+#cp -R NetBoot temp
+#cp -R SUS temp
+#cp -R webadmin temp
+#cp -R LDAPProxy temp
+#cp -R includes/* temp/base/
+#cp -R includes/* temp/NetBoot/
+#cp -R includes/* temp/SUS/
+#cp -R includes/* temp/webadmin/
+#cp -R includes/* temp/LDAPProxy/
 mkdir -p temp/installer/checks
 mkdir -p temp/installer/resources
 mkdir -p temp/installer/utils
@@ -42,11 +53,30 @@ if [ -x /usr/bin/xattr ]; then find temp -exec xattr -c {} \; ;fi # Remove OS X 
 find temp -name .DS_Store -delete # Clean out .DS_Store files
 find temp -name .svn | xargs rm -Rf # Clean out SVN garbage
 
+
+# Generate NetBoot App sub-installer
+#timeEcho "Creating NetBoot sub-installer..."
+#bash makeself/makeself.sh temp/NetBoot/ temp/base/netbootInstall.run "NetBoot Installer" "bash netbootInstall.sh" > /dev/null
+
+# Generate SUS sub-installer
+#timeEcho "Creating SUS sub-installer..."
+#bash makeself/makeself.sh temp/SUS/ temp/base/susInstall.run "SUS Installer" "bash susInstall.sh" > /dev/null
+
+# Generate webadmin sub-installer
+#timeEcho "Creating webadmin sub-installer..."
+#bash makeself/makeself.sh temp/webadmin/ temp/base/webadminInstall.run "WebAdmin Installer" "bash webadminInstall.sh" > /dev/null
+
+# Generate LDAP Proxy sub-installer
+#timeEcho "Creating LDAP Proxy sub-installer..."
+#bash makeself/makeself.sh temp/LDAPProxy/ temp/base/LDAPProxyInstall.run "LDAP Proxy Installer" "bash LDAPProxyInstall.sh" > /dev/null
+
 # Generate final installer
 timeEcho "Creating final installer..."
+#bash makeself/makeself.sh temp/base/ NetSUSLPInstaller.run "NetSUSLP Installer" "bash NetSUSInstaller.sh"
 bash makeself/makeself.sh temp/installer/ NetSUSLPInstaller.run "NetSUSLP Installer" "bash install.sh"
 
 timeEcho "Cleaning up..."
+#cp temp/*/*.run .  # Uncomment this if you want to test the sub-installers outside of the main installer
 rm -Rf temp 2>&1 > /dev/null
 timeEcho "Finished creating the NetSUS Installer.  "
 
