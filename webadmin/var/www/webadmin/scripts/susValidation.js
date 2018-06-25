91dc737159fdabc4e1e61cb958051d3c172370d1
Duncan McCracken <dcmccracken@me.com>
2018-06-25 17:48:25 +1000

    SUS Validation
    Removed unnecessary call for feedback offset

--- webadmin/var/www/webadmin/scripts/susValidation.js
+++ webadmin/var/www/webadmin/scripts/susValidation.js
@@ -85,7 +85,7 @@ function updateBaseUrl(element, offset = false) {
 	hideWarning(element);
 	if (/^http:\/\/(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[0-9][\/]|[1-9][0-9]|[1-9][0-9][\/]|1[0-9]{2}|1[0-9]{2}[\/]|2[0-4][0-9]|2[0-4][0-9][\/]|25[0-5]|25[0-5][\/])$|^http:\/\/(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][\/]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9][\/])$/.test(element.value)) {
 		ajaxPost('susCtl.php', 'baseurl='+element.value);
-		showSuccess(element, offset);
+		showSuccess(element);
 	}
 }
 
