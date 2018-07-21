ff41c65931870b953783dcf8362cd8b39209f033
Duncan McCracken <dcmccracken@me.com>
2018-07-21 18:50:14 +1000

    LDAP Proxy
    Added show / hide in dashboard function

--- webadmin/var/www/webadmin/ldapProxyCtl.php
+++ webadmin/var/www/webadmin/ldapProxyCtl.php
@@ -20,16 +20,18 @@ if (!($_SESSION['isAuthUser'])) {
 	if (isset($_POST['ldapproxy'])) {
 		if ($_POST['ldapproxy'] == "enable") {
 			$conf->setSetting("ldapproxy", "enabled");
+			ldapExec("enableproxy");
 		} else {
 			$conf->setSetting("ldapproxy", "disabled");
+			ldapExec("disableproxy");
 		}
 	}
 
-	if (isset($_POST['ldapservice'])) {
-		if ($_POST['ldapservice'] == "start") {
-			ldapExec("enableproxy");
+	if (isset($_POST['showproxy'])) {
+		if ($_POST['showproxy'] == "true") {
+			$conf->setSetting("showproxy", "true");
 		} else {
-			ldapExec("disableproxy");
+			$conf->setSetting("showproxy", "false");
 		}
 	}
 
--- webadmin/var/www/webadmin/ldapProxySettings.php
+++ webadmin/var/www/webadmin/ldapProxySettings.php
@@ -16,6 +16,11 @@ function ldapExec($cmd) {
 $ldap_running = (trim(ldapExec("getldapproxystatus")) === "true");
 
 ?>
+			<link rel="stylesheet" href="theme/awesome-bootstrap-checkbox.css"/>
+			<link rel="stylesheet" href="theme/bootstrap-toggle.css">
+
+			<script type="text/javascript" src="scripts/toggle/bootstrap-toggle.min.js"></script>
+
 			<script type="text/javascript">
 				function toggleService() {
 					if ($('#proxyenabled').prop('checked')) {
@@ -29,37 +34,33 @@ $ldap_running = (trim(ldapExec("getldapproxystatus")) === "true");
 					}
 				}
 				
-				function toggleLdap(element) {
-					if (element.checked) {
-						ajaxPost("ldapProxyCtl.php", "ldapservice=start");
-						$('#ldap_status').text('Running')
+				function toggleDashboard() {
+					if ($('#proxydashboard').prop('checked')) {
+						ajaxPost('ldapProxyCtl.php', 'showproxy=true');
 					} else {
-						ajaxPost("ldapProxyCtl.php", "ldapservice=stop");
-						$('#ldap_status').text('Not Running')
+						ajaxPost('ldapProxyCtl.php', 'showproxy=false');
 					}
 				}
 			</script>
 
-			<link rel="stylesheet" href="theme/awesome-bootstrap-checkbox.css"/>
-
 			<div class="description"><a href="settings.php">Settings</a> <span class="glyphicon glyphicon-chevron-right"></span> <span class="text-muted">Services</span> <span class="glyphicon glyphicon-chevron-right"></span></div>
-			<h2>LDAP Proxy</h2>
+			<div class="row">
+				<div class="col-xs-10"> 
+					<h2>LDAP Proxy</h2>
+				</div>
+				<div class="col-xs-2 text-right"> 
+					<input type="checkbox" id="proxyenabled" <?php echo ($conf->getSetting("ldapproxy") == "enabled" ? "checked" : ""); ?> data-toggle="toggle" onChange="toggleService();">
+				</div>
+			</div>
 
 			<div class="row">
 				<div class="col-xs-12"> 
 
 					<hr>
 
-					<div class="checkbox checkbox-primary" style="padding-top: 8px;">
-						<input name="proxyenabled" id="proxyenabled" class="styled" type="checkbox" value="true" onChange="toggleService();" <?php echo ($conf->getSetting("ldapproxy") == "enabled" ? "checked" : ""); ?>>
-						<label><strong>Enable LDAP Proxy</strong> <span style="font-size: 75%; color: #777;">DESCRIPTION</span></label>
-					</div>
-
-					<div class="service-settings">
-						<div class="checkbox checkbox-primary">
-							<input name="ldapproxystatus" id="ldapproxystatus" class="styled" type="checkbox" value="true" onChange="toggleLdap(this);" <?php echo ($ldap_running ? "checked" : ""); ?> <?php echo ($conf->getSetting("ldapproxy") == "enabled" ? "" : "disabled"); ?>>
-							<label><strong>LDAP Server</strong><br><span id="ldap_status" style="font-size: 75%; color: #777;"><?php echo ($ldap_running ? "Running" : "Not Running"); ?></span></label>
-						</div>
+					<div class="checkbox checkbox-primary" style="padding-top: 12px;">
+						<input name="proxydashboard" id="proxydashboard" class="styled" type="checkbox" value="true" onChange="toggleDashboard();" <?php echo ($conf->getSetting("showproxy") == "true" ? "checked" : ""); ?>>
+						<label><strong>Show in Dashboard</strong><br><span style="font-size: 75%; color: #777;">Display service status in the NetSUS dashboard.</span></label>
 					</div>
 
 				</div> <!-- /.col -->
