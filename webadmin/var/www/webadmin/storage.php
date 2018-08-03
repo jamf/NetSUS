7253060c4ddb3e56c98e5c46ce9e57cb1114c68e
Duncan McCracken <dcmccracken@me.com>
2018-08-03 14:09:44 +1000

    Storage
    Updated Layout with new styles

--- webadmin/var/www/webadmin/storage.php
+++ webadmin/var/www/webadmin/storage.php
@@ -46,85 +46,92 @@ if (strpos($lv_result, 'ERROR') === false) {
 				}
 			</style>
 
-			<div class="description"><a href="settings.php">Settings</a> <span class="glyphicon glyphicon-chevron-right"></span> <span class="text-muted">System</span> <span class="glyphicon glyphicon-chevron-right"></span></div>
-			<h2>Storage</h2>
+			<nav id="nav-title" class="navbar navbar-default navbar-fixed-top">
+				<div style="padding: 19px 20px 1px;">
+					<div class="description"><a href="settings.php">Settings</a> <span class="glyphicon glyphicon-chevron-right"></span> <span class="text-muted">System</span> <span class="glyphicon glyphicon-chevron-right"></span></div>
+					<h2>Storage</h2>
+				</div>
+			</nav>
 
-			<div class="row">
-				<div class="col-xs-12"> 
+			<div style="padding: 70px 20px 1px; background-color: #f9f9f9;">
+					<div class="text-muted" style="font-size: 12px; padding: 16px 0px;">STORAGE DESCRIPTION</div>
+			</div>
 
-					<hr>
+			<hr>
 
-					<div style="padding: 12px 0px;" class="description">STORAGE DESCRIPTION</div>
+			<div style="padding: 6px 20px 0px;">
+				<h5><strong>File System</strong></h5>
 
-					<h5><strong>File System</strong></h5>
-
-					<div class="row">
-						<div class="col-xs-4">
-							<div><span class="text-muted">Used:</span> <?php echo $df_used; ?></div>
-						</div>
-						<div class="col-xs-4">
-							<div class="text-center"><span class="text-muted">Reserved:</span> <?php echo $df_reserved; ?></div>
-						</div>
-						<div class="col-xs-4">
-							<div class="text-right"><span class="text-muted">Free:</span> <?php echo $df_free; ?></div>
-						</div>
+				<div class="row">
+					<div class="col-xs-4">
+						<div><span class="text-muted">Used:</span> <?php echo $df_used; ?></div>
 					</div>
-
-					<br>
-
-					<div class="progress">
-						<div class="progress-bar <?php echo ($df_used_percent >= 80 ? ($df_used_percent >= 90 ? "progress-bar-danger" : "progress-bar-warning") : ""); ?>" role="progressbar" aria-valuenow="<?php echo $df_used_percent; ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $df_used_percent; ?>%;"></div>
-						<div class="progress-bar <?php echo ($df_used_percent >= 80 ? ($df_used_percent >= 90 ? "progress-bar-danger-light" : "progress-bar-warning-light") : "progress-bar-light"); ?>" role="progressbar" aria-valuenow="<?php echo $df_reserved_percent; ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $df_reserved_percent; ?>%;"></div>
+					<div class="col-xs-4">
+						<div class="text-center"><span class="text-muted">Reserved:</span> <?php echo $df_reserved; ?></div>
 					</div>
+					<div class="col-xs-4">
+						<div class="text-right"><span class="text-muted">Free:</span> <?php echo $df_free; ?></div>
+					</div>
+				</div>
 
-					<hr>
-					<br>
+				<div class="progress" style="margin-bottom: 16px;">
+					<div class="progress-bar <?php echo ($df_used_percent >= 80 ? ($df_used_percent >= 90 ? "progress-bar-danger" : "progress-bar-warning") : ""); ?>" role="progressbar" aria-valuenow="<?php echo $df_used_percent; ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $df_used_percent; ?>%;"></div>
+					<div class="progress-bar <?php echo ($df_used_percent >= 80 ? ($df_used_percent >= 90 ? "progress-bar-danger-light" : "progress-bar-warning-light") : "progress-bar-light"); ?>" role="progressbar" aria-valuenow="<?php echo $df_reserved_percent; ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $df_reserved_percent; ?>%;"></div>
+				</div>
+			</div>
 
-					<h5><strong>Logical Volume</strong></h5>
+			<hr>
+
+			<div style="padding: 6px 20px 16px; background-color: #f9f9f9;">
+				<h5><strong>Logical Volume</strong></h5>
 <?php if (strpos($lv_result,'ERROR') !== false) { ?>
-					<div class="text-muted">
-						<span class="glyphicon glyphicon-exclamation-sign"></span> <?php echo str_replace('ERROR: ', '', $lv_result); ?>
+				<div style="margin-top: 13px; margin-bottom: 0px;" class="panel panel-primary">
+					<div class="panel-body">
+						<div class="text-muted"><span class="text-info glyphicon glyphicon-info-sign" style="padding-right: 12px;"></span><?php echo str_replace('ERROR: ', '', $lv_result); ?></div>
 					</div>
+				</div>
 <?php } else { ?>
-					<div class="row">
-						<div class="col-xs-4">
-							<div><span class="text-muted">Total:</span> <?php echo $lv_total; ?></div>
-						</div>
-						<div class="col-xs-4">
-							<div class="text-center"><span class="text-muted">Allocated:</span> <?php echo $lv_allocated; ?></div>
-						</div>
-						<div class="col-xs-4">
-							<div class="text-right"><span class="text-muted">Available:</span> <?php echo $lv_available; ?></div>
-						</div>
+				<div class="row">
+					<div class="col-xs-4">
+						<div><span class="text-muted">Total:</span> <?php echo $lv_total; ?></div>
 					</div>
-
-					<br>
-
-					<div class="progress">
-						<div class="progress-bar" role="progressbar" aria-valuenow="<?php echo $lv_percent; ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $lv_percent; ?>%;"></div>
+					<div class="col-xs-4">
+						<div class="text-center"><span class="text-muted">Allocated:</span> <?php echo $lv_allocated; ?></div>
 					</div>
-
-					<div class="text-right">
-						<button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#confirmExpand" <?php echo ($lv_percent < 100 ? "" : "disabled"); ?>>Expand</button>
+					<div class="col-xs-4">
+						<div class="text-right"><span class="text-muted">Available:</span> <?php echo $lv_available; ?></div>
 					</div>
+				</div>
 
-					<div class="modal fade" id="confirmExpand" tabindex="-1" role="dialog">
-						<div class="modal-dialog" role="document">
-							<div class="modal-content">
-								<div class="modal-header">
-									<h3 class="modal-title">Expand Volume</h3>
-								</div>
-								<div class="modal-body">
-									<div class="text-muted">Are you sure you want to expand the volume? The system will require a restart.</div>
-								</div>
-								<div class="modal-footer">
-									<button type="button" data-dismiss="modal" class="btn btn-default btn-sm pull-left">Cancel</button>
-									<button type="button" class="btn btn-primary btn-sm" onClick="document.location.href='storageCtl.php?resize=true';">Continue</button>
-								</div>
-							</div>
-						</div>
-					</div>
+				<div class="progress" style="margin-bottom: 16px;">
+					<div class="progress-bar" role="progressbar" aria-valuenow="<?php echo $lv_percent; ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $lv_percent; ?>%;"></div>
+				</div>
+
+				<div class="text-right">
+					<button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#confirmExpand" <?php echo ($lv_percent < 100 ? "" : "disabled"); ?>>Expand</button>
+				</div>
 <?php } ?>
-				</div> <!-- /.col -->
-			</div> <!-- /.row -->
+			</div>
+
+			<hr>
+
+			<!-- Expand Volume Modal -->
+			<div class="modal fade" id="confirmExpand" tabindex="-1" role="dialog">
+				<div class="modal-dialog" role="document">
+					<div class="modal-content">
+						<div class="modal-header">
+							<h3 class="modal-title">Expand Volume</h3>
+						</div>
+						<div class="modal-body">
+							<div class="text-muted">Are you sure you want to expand the volume? The system will require a restart.</div>
+						</div>
+						<div class="modal-footer">
+							<button type="button" data-dismiss="modal" class="btn btn-default btn-sm pull-left">Cancel</button>
+							<button type="button" class="btn btn-primary btn-sm" onClick="document.location.href='storageCtl.php?resize=true';">Continue</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- /.modal -->
+
 <?php include "inc/footer.php"; ?>
--- webadmin/var/www/webadmin/storageCtl.php
+++ webadmin/var/www/webadmin/storageCtl.php
@@ -12,21 +12,20 @@ if (!isset($_GET['resize'])) {
 } else {
 	include "inc/header.php";
 ?>
-			<div class="description"><a href="settings.php">Settings</a> <span class="glyphicon glyphicon-chevron-right"></span> <span class="text-muted">System</span> <span class="glyphicon glyphicon-chevron-right"></span> <a href="storage.php">Storage</a> <span class="glyphicon glyphicon-chevron-right"></span></div>
-			<h2>Expand Logical Volume</h2>
+			<nav id="nav-title" class="navbar navbar-default navbar-fixed-top">
+				<div style="padding: 19px 20px 1px;">
+					<div class="description"><a href="settings.php">Settings</a> <span class="glyphicon glyphicon-chevron-right"></span> <span class="text-muted">System</span> <span class="glyphicon glyphicon-chevron-right"></span> <a href="storage.php">Storage</a> <span class="glyphicon glyphicon-chevron-right"></span></div>
+					<h2>Expand Logical Volume</h2>
+				</div>
+			</nav>
 
-			<div class="row">
-				<div class="col-xs-12">
-
-					<hr>
-					<br>
-
+			<div style="padding: 80px 20px 16px; overflow-x: auto; background-color: #f9f9f9;">
 <?php
 $cmd = "sudo /bin/sh scripts/adminHelper.sh resizeDisk";
 while (@ ob_end_flush());
 $proc = popen($cmd, "r");
 ?>
-					<pre>
+				<pre style="background-color: #fff;">
 <?php
 while (!feof($proc)) {
 	echo fread($proc, 128);
@@ -34,10 +33,12 @@ while (!feof($proc)) {
 }
 ?></pre>
 
-					<button type="button" class="btn btn-primary btn-sm pull-right" data-toggle="modal" data-target="#restart-modal" onClick="restartModal();">Restart</button>
-					<button type="button" class="btn btn-default btn-sm pull-right" style="margin-right: 10px;" onClick="document.location.href='storage.php'">Done</button>
+				<div class="text-right">
+					<button type="button" class="btn btn-default btn-sm" onClick="document.location.href='storage.php'">Done</button>
+					<button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#restart-modal" onClick="restartModal();">Restart</button>
+				</div>
+			</div>
 
-				</div> <!-- /.col -->
-			</div> <!-- /.row -->
+			<hr>
 <?php }
 include "inc/footer.php"; ?>
