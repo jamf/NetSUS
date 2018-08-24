86f06aee6aac79b23cd39ca38fab5798d5aea63b
Duncan McCracken <dcmccracken@me.com>
2018-08-24 11:55:23 +1000

    Installer: Service Status
    Added code to determine service states during upgrade to ensure services are running post installation

--- base/NetSUSInstaller.sh
+++ base/NetSUSInstaller.sh
@@ -129,6 +129,46 @@ if sestatus | grep -q enforcing ; then
   setenforce permissive
 fi
 
+#==== Service Status ======================================#
+
+# Determine running services to (re)start post installation
+if [[ $(which update-rc.d 2>&-) != "" ]]; then
+  if service smbd status 2>&- | grep -q running; then
+    start_smb="true"
+  fi
+  if service tftpd-hpa status 2>/dev/null | grep -q running; then
+    start_tftp="true"
+  fi
+elif [[ $(which chkconfig 2>&-) != "" ]]; then
+  if service smb status 2>&- | grep -q running; then
+    start_smb="true"
+  fi
+  if [[ $(which systemctl 2>&-) != "" ]]; then
+    if systemctl status tftp | grep -q running; then
+      start_tftp="true"
+    fi
+  else
+    if service xinetd status | grep -q running && chkconfig | sed 's/[ \t]//g' | grep tftp | grep -q ':on'; then
+      start_tftp="true"
+    fi
+  fi
+fi
+if service pybsdp status 2>&- | grep -q running; then
+  start_bsdp="true"
+fi
+if ps acx | grep -v grep | grep -q dhcpd; then
+  start_bsdp="true"
+fi
+if ps acx | grep -v grep | grep -q afpd; then
+  start_afp="true"
+fi
+if ps acx | grep -v grep | grep -q nfsd; then
+  start_nfs="true"
+fi
+if ps acx | grep -v grep | grep -q slapd; then
+  start_ldap="true"
+fi
+
 #==== Install Components ==================================#
 
 bash install-webadmin.sh || clean-fail
@@ -145,11 +185,6 @@ if grep -q 'net.ipv6.conf.lo.disable_ipv6' /etc/sysctl.conf; then
   sed -i '/net.ipv6.conf.default.disable_ipv6/d' /etc/sysctl.conf
   sed -i '/net.ipv6.conf.lo.disable_ipv6/d' /etc/sysctl.conf
 fi
-#echo "
-## Disable IPv6
-#net.ipv6.conf.all.disable_ipv6 = 1
-#net.ipv6.conf.default.disable_ipv6 = 1
-#" >> /etc/sysctl.conf
 
 log ""
 log "The NetSUSLP has been installed."
@@ -163,37 +198,137 @@ fi
 
 log "To complete the installation, open a web browser and navigate to https://${HOSTNAME}:443/."
 
+if [ -f "/etc/network/if-up.d/configurefornetboot" ]; then
+  rm -f /etc/network/if-up.d/configurefornetboot
+fi
+if [ -f "/sbin/ifup-local" ]; then
+  rm -f /sbin/ifup-local
+fi
+killall dhcpd > /dev/null 2>&1
 if [[ $(which update-rc.d 2>&-) != "" ]]; then
   service apparmor restart >> $logFile 2>&1
   service apache2 restart >> $logFile 2>&1
-  service slapd stop >> $logFile 2>&1
-  service netatalk stop >> $logFile 2>&1
-  service smbd stop >> $logFile 2>&1
-  service tftpd-hpa stop >> $logFile 2>&1
-  # service openbsd-inetd stop >> $logFile 2>&1
-  update-rc.d slapd disable >> $logFile 2>&1
-  update-rc.d netatalk disable >> $logFile 2>&1
-  if [[ $(which systemctl 2>&-) != "" ]]; then
-    update-rc.d smbd disable >> $logFile 2>&1
-    update-rc.d tftpd-hpa disable >> $logFile 2>&1
-    systemctl disable nfs-server >> $logFile 2>&1
-    # systemctl disable openbsd-inetd >> $logFile 2>&1
-    service nfs-server stop >> $logFile 2>&1
+  if [[ $start_smb == "true" ]]; then
+    if [[ $(which systemctl 2>&-) != "" ]]; then
+      update-rc.d smbd enable >> $logFile 2>&1
+    else
+      rm -f /etc/init/smbd.override
+    fi
+    service smbd start >> $logFile 2>&1
   else
-    echo manual > /etc/init/smbd.override
-    echo manual > /etc/init/tftpd-hpa.override
-    update-rc.d nfs-kernel-server disable >> $logFile 2>&1
-    # update-rc.d openbsd-inetd disable >> $logFile 2>&1
-    service nfs-kernel-server stop >> $logFile 2>&1
+    if [[ $(which systemctl 2>&-) != "" ]]; then
+      update-rc.d smbd disable >> $logFile 2>&1
+    else
+      echo manual > /etc/init/smbd.override
+    fi
+    service smbd stop >> $logFile 2>&1
+  fi
+  if [[ $start_tftp == "true" ]]; then
+    if [ "$(which systemctl 2>&-)" != '' ]; then
+      update-rc.d tftpd-hpa enable >> $logFile 2>&1
+    else
+      rm -f /etc/init/tftpd-hpa.override
+	fi
+    service tftpd-hpa start >> $logFile 2>&1
+  else
+    if [[ $(which systemctl 2>&-) != "" ]]; then
+      update-rc.d tftpd-hpa disable >> $logFile 2>&1
+    else
+      echo manual > /etc/init/tftpd-hpa.override
+    fi
+    service tftpd-hpa stop >> $logFile 2>&1
+  fi
+  if [[ $start_bsdp == "true" ]]; then
+	update-rc.d pybsdp enable >> $logFile 2>&1
+	service pybsdp start >> $logFile 2>&1
+  else
+	update-rc.d pybsdp disable >> $logFile 2>&1
+	service pybsdp stop >> $logFile 2>&1
+  fi
+  if [[ $start_afp == "true" ]]; then
+	update-rc.d netatalk enable >> $logFile 2>&1
+    service netatalk start >> $logFile 2>&1
+  else
+    update-rc.d netatalk disable >> $logFile 2>&1
+    service netatalk stop >> $logFile 2>&1
+  fi
+  if [[ $start_nfs == "true" ]]; then
+    if [[ $(which systemctl 2>&-) != "" ]]; then
+      systemctl enable nfs-server >> $logFile 2>&1
+      service nfs-server start >> $logFile 2>&1
+    else
+      update-rc.d nfs-kernel-server enable >> $logFile 2>&1
+      service nfs-kernel-server start >> $logFile 2>&1
+    fi
+  else
+    if [[ $(which systemctl 2>&-) != "" ]]; then
+      systemctl disable nfs-server >> $logFile 2>&1
+      service nfs-server stop >> $logFile 2>&1
+    else
+      update-rc.d nfs-kernel-server disable >> $logFile 2>&1
+      service nfs-kernel-server stop >> $logFile 2>&1
+    fi
+  fi
+  if [[ $start_ldap == "true" ]]; then
+    update-rc.d slapd enable >> $logFile 2>&1
+    service slapd start >> $logFile 2>&1
+  else
+    update-rc.d slapd disable >> $logFile 2>&1
+    service slapd stop >> $logFile 2>&1
   fi
   log "If you are installing NetSUSLP for the first time, please follow the documentation for setup instructions."
 elif [[ $(which chkconfig 2>&-) != "" ]]; then
   service httpd restart >> $logFile 2>&1
-  chkconfig tftp off >> $logFile 2>&1
-  chkconfig nfs off > /dev/null 2>&1
-  #if [ -f "/etc/sysconfig/xinetd" ]; then
-  #  service xinetd restart >> $logFile 2>&1
-  #fi
+  if [[ $start_smb == "true" ]]; then
+    chkconfig smb on >> $logFile 2>&1
+    service smb start >> $logFile 2>&1
+  else
+    chkconfig smb off >> $logFile 2>&1
+    service smb stop >> $logFile 2>&1
+  fi
+  if [[ $start_tftp == "true" ]]; then
+    chkconfig tftp on >> $logFile 2>&1
+    if [ "$(which systemctl 2>&-)" != '' ]; then
+      service tftp start >> $logFile 2>&1
+     else
+       service xinetd restart >> $logFile 2>&1
+     fi
+  else
+    chkconfig tftp off >> $logFile 2>&1
+    if [ "$(which systemctl 2>&-)" != '' ]; then
+      service tftp stop >> $logFile 2>&1
+     else
+       service xinetd restart >> $logFile 2>&1
+     fi
+  fi
+  if [[ $start_bsdp == "true" ]]; then
+    chkconfig pybsdp on >> $logFile 2>&1
+    service pybsdp start >> $logFile 2>&1
+  else
+    chkconfig pybsdp off >> $logFile 2>&1
+    service pybsdp stop >> $logFile 2>&1
+  fi
+  if [[ $start_afp == "true" ]]; then
+    chkconfig netatalk on >> $logFile 2>&1
+    service netatalk start >> $logFile 2>&1
+  else
+    chkconfig netatalk off >> $logFile 2>&1
+    service netatalk stop >> $logFile 2>&1
+  fi
+  if [[ $start_nfs == "true" ]]; then
+    chkconfig nfs on >> $logFile 2>&1
+    service nfs start >> $logFile 2>&1
+  else
+    chkconfig nfs off >> $logFile 2>&1
+    service nfs stop >> $logFile 2>&1
+  fi
+  if [[ $start_ldap == "true" ]]; then
+    chkconfig slapd on >> $logFile 2>&1
+    service slapd start >> $logFile 2>&1
+  else
+    chkconfig slapd off >> $logFile 2>&1
+    service slapd stop >> $logFile 2>&1
+  fi
   log "If you are installing NetSUSLP for the first time, please follow the documentation for setup instructions."
 fi
 
