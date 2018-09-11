21d5d3a0b849400b39428728a4d49e9443a146f3
Duncan McCracken <dcmccracken@me.com>
2018-09-11 20:53:19 +1000

    Bug Fix: LDAP Proxy
    php error log was showing slapd_error as an undefined call, updated test which references variable to use isset

--- webadmin/var/www/webadmin/proxySettings.php
+++ webadmin/var/www/webadmin/proxySettings.php
@@ -89,7 +89,7 @@ if ($conf->getSetting("ldapproxy") == "enabled" && sizeof($conf->getProxies()) >
 						</div>
 					</div>
 
-					<div id="slapd_error" style="margin-top: 9px; margin-bottom: 17px; border-color: #d43f3a;" class="panel panel-danger <?php echo (empty($slapd_error) ? "hidden" : ""); ?>">
+					<div id="slapd_error" style="margin-top: 9px; margin-bottom: 17px; border-color: #d43f3a;" class="panel panel-danger <?php echo (isset($slapd_error) ? "" : "hidden"); ?>">
 						<div class="panel-body">
 							<input type="hidden" id="enableproxy" name="enableproxy" value="">
 							<div class="text-muted"><span class="text-danger glyphicon glyphicon-exclamation-sign" style="padding-right: 12px;"></span><?php echo $slapd_error; ?></div>
